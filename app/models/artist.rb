7faf316220c5e595e92a0c643ba32e25c764c258
github username <github email address>
2020-05-23 17:34:05 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,14 +3,21 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    #return the genre of the artist's first saved song
+    # Returns the genre of the artist's first saved song (maybe the #get_first_song method can be used here?)
+    Genre.all.find do |genre|
+      genre.id == self.songs.first.genre_id
+    end
   end
 
   def song_count
     #return the number of songs associated with the artist
+    self.songs.count
   end
 
   def genre_count
     #return the number of genres associated with the artist
+    Genre.all.find_all do |genre|
+      genre.id == self.songs.first.genre_id
+    end.count
   end
 end
